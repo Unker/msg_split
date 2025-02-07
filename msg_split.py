7becc8a5d401d2f815b9b5d1a60fdd73d16ffe68
unker <kfi.unknown@gmail.com>
2025-02-08 02:22:39 +0300

    rename variables

--- msg_split.py
+++ msg_split.py
@@ -1,165 +1,153 @@
 from lxml import etree
-from lxml import html
 from typing import Generator, List
 import argparse
 
 MAX_LEN = 4096
-stack: List[str] = []  # Стек для отслеживания открытых тегов
-stack_cl: List[str] = []  # Стек для отслеживания закрытых тегов
+open_tags_stack: List[str] = []  # Стек для отслеживания открытых тегов
+close_tags_stack: List[str] = []  # Стек для отслеживания закрытых тегов
+
 
 def split_message(source: str, max_len=MAX_LEN) -> Generator[str, None, None]:
-  """Splits the original message (`source`) into fragments of the specified length
-  (`max_len`)."""
-  def get_open_tag(element):
-      """Формирует открывающий тег с атрибутами."""
-      open_tag = f"<{element.tag}"
-      for attr, value in element.attrib.items():
-          open_tag += f' {attr}="{value}"'
-      open_tag += ">"
-      return open_tag
+    """Splits the original message (`source`) into fragments of the specified length
+    (`max_len`)."""
 
-  def get_close_tag(element):
-      """Формирует закрывающий тег."""
-      return f"</{element.tag}>"
+    def get_open_tag(element):
+        """Формирует открывающий тег с атрибутами."""
+        tag = f"<{element.tag}"
+        for attr, value in element.attrib.items():
+            tag += f' {attr}="{value}"'
+        tag += ">"
+        return tag
 
-  def get_chunk(element, max_len, chunk=""):
-      """Рекурсивно формирует фрагмент сообщения."""
-      nonlocal stack, stack_cl, is_full
+    def get_close_tag(element):
+        """Формирует закрывающий тег."""
+        return f"</{element.tag}>"
 
-      if element.tag == 'body':
-        if element.text:
-          if len(chunk) + len(element.text) > max_len:
-            is_full = True
-            return element.text
-          else:
-            chunk += element.text
+    def get_chunk(element, max_len, current_chunk=""):
+        """Рекурсивно формирует фрагмент сообщения."""
+        nonlocal open_tags_stack, close_tags_stack, is_full
 
-        # Обходим дочерние элементы
-        for child in element:
-          chunk = get_chunk(child, max_len, chunk)
-          if is_full:
-            break
-        
-      else:
-        open_tag = get_open_tag(element)
-        close_tag = get_close_tag(element)
-        stack.append(open_tag)
-        stack_cl.append(close_tag)
+        if element.tag == "body":
+            if element.text:
+                if len(current_chunk) + len(element.text) > max_len:
+                    is_full = True
+                    return element.text
+                else:
+                    current_chunk += element.text
 
-        sz_close_tags = sum(len(s) for s in stack_cl)
-
-        # Пытаемся добавить весь блок
-        html_text = etree.tostring(element, encoding='unicode', method='html')
-        sz_total = len(chunk) + len(html_text) + sz_close_tags
-        if sz_total < max_len:
-            chunk += html_text
-            stack_cl.pop()
-            stack.pop()
-            return chunk
+            # Обходим дочерние элементы
+            for child in element:
+                current_chunk = get_chunk(child, max_len, current_chunk)
+                if is_full:
+                    break
         else:
-          # Если нет потомком, то это должен быть последний блок в chunk
-          if len(element) == 0:
-            # отсекаем хвост
-            tail = max_len-sz_total
-            tail_text = html_text[:tail]
-            chunk += tail_text
-            stack_cl.pop()
-            stack.pop()
-            is_full = True
-            return chunk
+            open_tag = get_open_tag(element)
+            close_tag = get_close_tag(element)
+            open_tags_stack.append(open_tag)
+            close_tags_stack.append(close_tag)
 
-        # Пытаемся добавить открывающий тег
-        if len(chunk) + len(open_tag) + sz_close_tags > max_len:
-            stack_cl.pop()
-            stack.pop()
-            is_full = True
-            return chunk
-        else:
-            chunk += open_tag
+            total_close_tags_length = sum(len(tag) for tag in close_tags_stack)
 
-        # Пытаемся добавить текст элемента
-        if element.text:
-            if len(chunk) + len(element.text) + sz_close_tags > max_len:
-                is_full = True
-                return chunk
+            # Пытаемся добавить весь блок
+            html_text = etree.tostring(element, encoding="unicode", method="html")
+            total_length = len(current_chunk) + len(html_text) + total_close_tags_length
+            if total_length < max_len:
+                current_chunk += html_text
+                close_tags_stack.pop()
+                open_tags_stack.pop()
+                return current_chunk
             else:
-                chunk += element.text
+                # Если нет потомков, то это должен быть последний блок в current_chunk
+                if len(element) == 0:
+                    # Отсекаем хвост
+                    tail_length = max_len - total_length
+                    tail_text = html_text[:tail_length]
+                    current_chunk += tail_text
+                    close_tags_stack.pop()
+                    open_tags_stack.pop()
+                    is_full = True
+                    return current_chunk
 
-        # Обходим дочерние элементы
-        for child in element:
-            if is_full:
-                break
-            chunk = get_chunk(child, max_len, chunk)
+            # Пытаемся добавить открывающий тег
+            if len(current_chunk) + len(open_tag) + total_close_tags_length > max_len:
+                close_tags_stack.pop()
+                open_tags_stack.pop()
+                is_full = True
+                return current_chunk
+            else:
+                current_chunk += open_tag
 
-      # if not is_full:
-      #     chunk += close_tag
-      #     stack_cl.pop()
-      #     stack.pop()
+            # Пытаемся добавить текст элемента
+            if element.text:
+                if (
+                    len(current_chunk) + len(element.text) + total_close_tags_length
+                    > max_len
+                ):
+                    is_full = True
+                    return current_chunk
+                else:
+                    current_chunk += element.text
 
-      return chunk
+            # Обходим дочерние элементы
+            for child in element:
+                if is_full:
+                    break
+                current_chunk = get_chunk(child, max_len, current_chunk)
 
-  stack = []
-  stack_cl = []
-  is_full = False
+        return current_chunk
 
-  while True:
-      if len(source) < max_len:
-          yield source
-          break
+    open_tags_stack = []
+    close_tags_stack = []
+    is_full = False
 
-      # Парсим HTML
-      tree = etree.fromstring(f"<body>{source}</body>")
-      body_element = tree.xpath("//body")[0]
+    while True:
+        if len(source) < max_len:
+            yield source
+            break
 
-      chunk = get_chunk(body_element, max_len)
-      chunk = chunk.replace("<body>", "").replace("</body>", "")
+        # Парсим HTML
+        tree = etree.fromstring(f"<body>{source}</body>")
+        body_element = tree.xpath("//body")[0]
 
-      # Удаляем обработанную часть из исходного элемента
-      source = source.replace(chunk, "", 1)
+        chunk = get_chunk(body_element, max_len)
+        chunk = chunk.replace("<body>", "").replace("</body>", "")
 
-      if not source:
-          break
+        # Удаляем обработанную часть из исходного элемента
+        source = source.replace(chunk, "", 1)
 
-      # Закрываем все открытые теги
-      close_tags = "".join(reversed(stack_cl))
-      chunk += close_tags
+        if not source:
+            break
 
-      yield chunk
+        # Закрываем все открытые теги
+        close_tags = "".join(reversed(close_tags_stack))
+        chunk += close_tags
 
-      is_full = False
-      # Открываем тэги в новом фрагменте
-      open_tags = "".join(stack)
-      stack = []
-      stack_cl = []
-      source = open_tags + source
+        yield chunk
+
+        is_full = False
+        # Открываем тэги в новом фрагменте
+        open_tags = "".join(open_tags_stack)
+        open_tags_stack = []
+        close_tags_stack = []
+        source = open_tags + source
 
 
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Split HTML message into fragments.")
+    parser.add_argument(
+        "--max-len", type=int, default=MAX_LEN, help="Maximum length of each fragment"
+    )
+    parser.add_argument("file", type=str, help="Path to the HTML file")
 
+    args = parser.parse_args()
 
+    with open(args.file, "r", encoding="utf-8") as file:
+        source = file.read()
 
-if __name__ == '__main__':
-  parser = argparse.ArgumentParser(description="Split HTML message into fragments.")
-  parser.add_argument('--max-len', type=int, default=MAX_LEN, help="Maximum length of each fragment")
-  parser.add_argument('file', type=str, help="Path to the HTML file")
-
-  args = parser.parse_args()
-  
-  with open(args.file, 'r', encoding='utf-8') as file:
-      source = file.read()
-
-  # tree = etree.HTML(source)
-  # body_content = tree.find('.//body')
-
-  # for fragment in split_message(source, max_len=200):
-  #   print("Фрагмент:")
-  #   print(fragment)
-  #   print("-" * 40)
-
-  
-  try:
-      for i, fragment in enumerate(split_message(source, args.max_len), 1):
-          print(f"fragment #{i}: {len(fragment)} chars")
-          print(fragment)
-          print("-" * 40)
-  except ValueError as e:
-      print(f"Error: {e}")
+    try:
+        for i, fragment in enumerate(split_message(source, args.max_len), 1):
+            print(f"fragment #{i}: {len(fragment)} chars")
+            print(fragment)
+            print("-" * 40)
+    except ValueError as e:
+        print(f"Error: {e}")
